e02ff321cc5639a0cdfc129d0215a4f1833fe3a0
旅游报价系统备份 <backup@local>
2026-08-15 18:43:36 +0800

    style: compact quote editor value columns

--- tests/Feature/Views/WorkspaceViewContractTest.php
+++ tests/Feature/Views/WorkspaceViewContractTest.php
@@ -199,6 +199,20 @@ class WorkspaceViewContractTest extends TestCase
         );
     }
 
+    public function test_quote_editor_gives_notes_more_space_than_compact_value_columns(): void
+    {
+        $styles = file_get_contents(public_path('css/workspace.css'));
+
+        $this->assertStringContainsString(
+            'grid-template-columns: 24px 25px minmax(82px,.55fr) minmax(145px,1.25fr) minmax(58px,.45fr) 48px 72px 82px minmax(170px,1.65fr) 28px;',
+            $styles
+        );
+        $this->assertStringContainsString(
+            'grid-template-columns: 22px 23px minmax(80px,.5fr) minmax(130px,1.15fr) 55px 48px 68px 78px minmax(150px,1.45fr) 27px;',
+            $styles
+        );
+    }
+
     public function test_spreadsheet_export_contains_document_metadata_details_and_totals(): void
     {
         $document = file_get_contents(resource_path('views/quotes/_document.blade.php'));
